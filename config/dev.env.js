c7ae4a1f1825cc83564dc31ec2849d1fea0c20d4
PKTseng <jk0910948659@Gmail.com>
2021-03-15 16:53:08 +0800

    Add env location and axios

--- config/dev.env.js
+++ config/dev.env.js
@@ -1,7 +1,10 @@
-'use strict'
-const merge = require('webpack-merge')
-const prodEnv = require('./prod.env')
+"use strict";
+const merge = require("webpack-merge");
+const prodEnv = require("./prod.env");
 
 module.exports = merge(prodEnv, {
-  NODE_ENV: '"development"'
-})
+  NODE_ENV: '"development"',
+  APIPATH: '"https://vue-course-api.hexschool.io"', // 這是伺服器路徑
+  THEO: '"http://localhost:3000"', // 這是伺服器路徑
+  COSTOMPATH: '"pkt"' // 這是自定義的路徑
+});
