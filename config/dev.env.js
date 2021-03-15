8e2862e93d860e9e7c8d402edc2ffc1130ed8b14
PKTseng <jk0910948659@Gmail.com>
2021-03-15 19:04:02 +0800

    Add theo api url

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,7 +4,5 @@ const prodEnv = require("./prod.env");
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  APIPATH: '"https://vue-course-api.hexschool.io"', // 這是伺服器路徑
-  THEO: '"http://localhost:3000"', // 這是伺服器路徑
-  COSTOMPATH: '"pkt"' // 這是自定義的路徑
+  THEOAPI: '"http://localhost:3000"' // 這是伺服器路徑s
 });
