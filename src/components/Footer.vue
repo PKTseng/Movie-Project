9b51013b20dd911ef2f560f02adac73238e6e519
PKTseng <jk0910948659@Gmail.com>
2021-03-15 14:05:00 +0800

    Add footer components

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,20 @@
 <template>
-  <div></div>
+  <div>
+    <footer class="footer py-3 text-muted mt-4">
+      <div class="container"></div>
+      <ul class="list-inline d-flex justify-content-center align-items-center">
+        <li class="list-inline-item">
+          © Copyright 2021 平價電影出租 純屬練習作品
+        </li>
+        <!-- <li class="list-inline-item">
+          <a href="javascript::" class="text-info"></a>
+        </li>
+        <li class="list-inline-item">
+          <a href="javascript::" class="text-info"></a>
+        </li> -->
+      </ul>
+    </footer>
+  </div>
 </template>
 
 <script>
@@ -8,4 +23,14 @@ export default {
 };
 </script>
 
-<style></style>
+<style scoped lang="scss">
+// footer
+.footer {
+  font-size: 20px;
+  background: #1e1e1e;
+  @media (max-width: 768px) {
+    font-size: 14px;
+    margin-bottom: 50px;
+  }
+}
+</style>
