c1e969a9bb59fc894b7aab4416fe006a53f7c6c5
PKTseng <jk0910948659@Gmail.com>
2021-03-16 16:45:13 +0800

    Add swiper components

---
+++ src/components/MovieBanner.vue
@@ -0,0 +1,302 @@
+<template>
+  <div>
+    <!-- Banner 大圖 -->
+    <div class="swiper-container gallery-top">
+      <div class="swiper-wrapper">
+        <!--  banner 第1張 -->
+        <div class="swiper-slide banner1">
+          <div class="container h-100">
+            <div class="row text-white d-flex align-items-center h-100">
+              <div class="col-md-6">
+                <span class="badge badge-success p-2">冒險</span>
+                <h2 class="bannerTitle">魔物獵人</h2>
+                <p class="bannerInfo">
+                  一支聯合國部隊進入了通往另一個世界的穿越口，並在那協同其住民與巨型怪物戰鬥。該世界的人類必須與聯合國部隊們共同努力守護其穿越口，以防止怪物透過入口來侵襲地球。
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!--  banner 第2張 -->
+        <div class="swiper-slide banner2">
+          <div class="container h-100">
+            <div class="row text-white d-flex align-items-center h-100">
+              <div class="col-md-6">
+                <span class="badge badge-success p-2">冒險</span>
+                <h2 class="bannerTitle">白頭山：半島浩劫</h2>
+                <p class="bannerInfo">
+                  2019
+                  年韓國災難電影，由李海準、金丙書執導，河正宇、李炳憲、馬東石、田慧振、裴秀智主演。
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!--  banner 第3張 -->
+        <div class="swiper-slide banner3">
+          <div class="container h-100">
+            <div class="row text-white d-flex align-items-center h-100">
+              <div class="col-md-6">
+                <span class="badge badge-success p-2">劇情</span>
+                <h2 class="bannerTitle">異種獵殺</h2>
+                <p class="bannerInfo">
+                  在一艘目標潛網「新地球」的跨星際太空船上，一位即將成為人父的機械技師必須對抗不懷好意的星際恐怖力量，他們企圖將這太空船轉變成致命武器。
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!--  banner 第4張 -->
+        <div class="swiper-slide banner4">
+          <div class="container h-100">
+            <div class="row text-white d-flex align-items-center h-100">
+              <div class="col-md-6">
+                <span class="badge badge-success p-2">動作</span>
+                <h2 class="bannerTitle">柔道煞星</h2>
+                <p class="bannerInfo">
+                  一部2020年美國奇幻動作片，由迪米泰瑞·洛戈塞蒂斯執導並與吉姆·麥格拉斯（Jim
+                  McGrath）共同編劇，改編自兩人創作的2017年同名漫畫；故事描述一群古代柔術高手將共同對抗威脅地球的外星入侵者。
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!--  banner 第5張 -->
+        <div class="swiper-slide banner5">
+          <div class="container h-100">
+            <div class="row text-white d-flex align-items-center h-100">
+              <div class="col-md-6">
+                <span class="badge badge-success p-2">動作</span>
+                <h2 class="bannerTitle">尋龍使者：拉雅</h2>
+                <p class="bannerInfo">
+                  故事講述一個孤獨的勇士開始尋找世界上最後的一條龍，並從邪惡的德魯恩（Druun）手上拯救已經分裂為五個不同區域的奇幻王國「庫曼德拉」（Kumandra）。
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!--  banner 第6張 -->
+        <div class="swiper-slide banner6">
+          <div class="container h-100">
+            <div class="row text-white d-flex align-items-center h-100">
+              <div class="col-md-6">
+                <span class="badge badge-success p-2">冒險</span>
+                <h2 class="bannerTitle">史酷比！劍與史酷比</h2>
+                <p class="bannerInfo">
+                  一個邪惡的女巫將幫派運送回騎士時代的騎士，法術鑄造巫師和噴火龍。
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!--  banner 第7張 -->
+        <div class="swiper-slide banner7">
+          <div class="container h-100">
+            <div class="row text-white d-flex align-items-center h-100">
+              <div class="col-md-6">
+                <span class="badge badge-success p-2">冒險</span>
+                <h2 class="bannerTitle">古魯家族2：新石代</h2>
+                <p class="bannerInfo">
+                  《古魯家族：新石代》是一部2020年上映的美國3D電腦動畫冒險喜劇電影，由夢工廠動畫公司製作、環球影業發行。
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!--  banner 第8張 -->
+        <div class="swiper-slide banner8">
+          <div class="container h-100">
+            <div class="row text-white d-flex align-items-center h-100">
+              <div class="col-md-6">
+                <span class="badge badge-success p-2">動作</span>
+                <h2 class="bannerTitle">TENET 天能</h2>
+                <p class="bannerInfo">
+                  《TENET天能》是一部於2020年上映英國與美國合拍的科幻動作驚悚片，由克里斯多福·諾蘭編劇和執導。主演包括約翰·大衛·華盛頓、羅伯·派汀森、伊莉莎白·戴比基、蒂普·卡柏迪亞、米高·肯恩和肯尼斯·布萊納。
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!--  banner 第9張 -->
+        <div class="swiper-slide banner9">
+          <div class="container h-100">
+            <div class="row text-white d-flex align-items-center h-100">
+              <div class="col-md-6">
+                <span class="badge badge-success p-2">冒險</span>
+                <h2 class="bannerTitle">龍之心：致命復仇</h2>
+                <p class="bannerInfo">
+                  由伊凡·西爾維斯特里尼執導的2020年美國-英國-羅馬尼亞奇幻冒險電影。這是1996年電影《魔龍傳奇》的第三部非戲院首映前傳。影片在《龍之心4：心火之戰》之前開始，但在此之後結束。
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!--  banner 第10張 -->
+        <div class="swiper-slide banner10">
+          <div class="container h-100">
+            <div class="row text-white d-flex align-items-center h-100">
+              <div class="col-md-6">
+                <span class="badge badge-success p-2">奇幻</span>
+                <h2 class="bannerTitle">勝利號</h2>
+                <p class="bannerInfo">
+                  《勝利號》是一部2021年韓國太空冒險電影，由趙成熙執導，宋仲基、金泰梨、陳善奎和柳海真主演，為韓國首部太空電影。
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- Add Arrows -->
+      <div class="swiper-button-next swiper-button-white"></div>
+      <div class="swiper-button-prev swiper-button-white"></div>
+    </div>
+    <!-- Banner 小圖 -->
+    <div class="swiper-container gallery-thumbs">
+      <div class="swiper-wrapper downMovie">
+        <!--  banner 第1張 -->
+        <div class="swiper-slide banner1"></div>
+        <!--  banner 第2張 -->
+        <div class="swiper-slide banner2"></div>
+        <!--  banner 第3張 -->
+        <div class="swiper-slide banner3"></div>
+        <!--  banner 第4張 -->
+        <div class="swiper-slide banner4"></div>
+        <!--  banner 第5張 -->
+        <div class="swiper-slide banner5"></div>
+        <!--  banner 第6張 -->
+        <div class="swiper-slide banner6"></div>
+        <!--  banner 第7張 -->
+        <div class="swiper-slide banner7"></div>
+        <!--  banner 第8張 -->
+        <div class="swiper-slide banner8"></div>
+        <!--  banner 第9張 -->
+        <div class="swiper-slide banner9"></div>
+        <!--  banner 第10張 -->
+        <div class="swiper-slide banner10"></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { Swiper, SwiperSlide } from "vue-awesome-swiper";
+import "swiper/css/swiper.css";
+
+export default {
+  name: "MovieBanner",
+  components: {
+    Swiper,
+    SwiperSlide
+  },
+
+  mounted() {
+    const galleryThumbs = new Swiper(".gallery-thumbs", {
+      spaceBetween: 10,
+      slidesPerView: 4,
+      loop: true,
+      freeMode: true,
+      loopedSlides: 10, //looped slides should be the same
+      watchSlidesVisibility: true,
+      watchSlidesProgress: true,
+      speed: 1000
+      // autoplay: {
+      //   delay: 2000,
+      //   disableOnInteraction: true,
+      // },
+    });
+    const galleryTop = new Swiper(".gallery-top", {
+      spaceBetween: 10,
+      loop: true,
+      loopedSlides: 10, //looped slides should be the same
+      speed: 1000,
+      // autoplay: {
+      //   delay: 2000,
+      //   disableOnInteraction: true,
+      // },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev"
+      },
+      thumbs: {
+        swiper: galleryThumbs
+      }
+    });
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+// 整個 swiper 設定
+.swiper-container {
+  margin-left: auto;
+  margin-right: auto;
+  width: 100%;
+  .swiper-slide {
+    background-size: cover;
+    background-position: center center;
+    background-repeat: no-repeat;
+    .bannerTitle {
+      font-size: 56px;
+      @media (max-width: 768px) {
+        font-size: 36px;
+        align-items: start;
+      }
+    }
+    .bannerInfo {
+      font-size: 18px;
+    }
+  }
+  .banner1 {
+    background-image: url("../assets/banner/banner1.jpg");
+  }
+  .banner2 {
+    background-image: url("../assets/banner/banner2.jpg");
+  }
+  .banner3 {
+    background-image: url("../assets/banner/banner3.jpg");
+  }
+  .banner4 {
+    background-image: url("../assets/banner/banner4.jpg");
+  }
+  .banner5 {
+    background-image: url("../assets/banner/banner5.jpg");
+  }
+  .banner6 {
+    background-image: url("../assets/banner/banner6.jpg");
+  }
+  .banner7 {
+    background-image: url("../assets/banner/banner7.jpg");
+  }
+  .banner8 {
+    background-image: url("../assets/banner/banner8.jpg");
+  }
+  .banner9 {
+    background-image: url("../assets/banner/banner9.jpg");
+  }
+  .banner10 {
+    background-image: url("../assets/banner/banner10.jpg");
+  }
+}
+
+// swiper 大圖設定
+.gallery-top {
+  width: 100%;
+  height: 450px;
+}
+
+// swiper 小圖設定
+.gallery-thumbs {
+  height: 15vh;
+  box-sizing: border-box;
+  padding: 10px 0;
+}
+
+.gallery-thumbs .swiper-slide {
+  height: 100%;
+  opacity: 0.4;
+}
+
+.gallery-thumbs .swiper-slide-thumb-active {
+  opacity: 1;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,25 @@
 <template>
   <div>
     <Header></Header>
+    <MovieBanner></MovieBanner>
+    <!-- <MovieCard></MovieCard> -->
+    <Footer></Footer>
   </div>
 </template>
 
 <script>
 import Header from "@/components/Navbar";
+import Footer from "@/components/Footer";
+import MovieCard from "@/components/MovieCard";
+import MovieBanner from "@/components/MovieBanner";
+
 export default {
   name: "index",
   components: {
-    Header
+    Header,
+    Footer,
+    MovieCard,
+    MovieBanner
   },
   data() {
     return {
@@ -19,4 +29,4 @@ export default {
 };
 </script>
 
-<style></style>
+<style scoped lang="scss"></style>
