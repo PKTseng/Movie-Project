d4c1996c576728f0d24121e448588ad57808a79f
PKTseng <jk0910948659@Gmail.com>
2021-03-19 13:57:37 +0800

    Refactor import

--- src/main.js
+++ src/main.js
@@ -1,33 +1,33 @@
 // 全域載入
-import Vue from "vue";
-import App from "./App";
-import router from "./router";
-import axios from "axios";
-import VueAxios from "vue-axios";
-import currencyFilter from "./filter/currency";
-window.Vue = Vue;
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import currencyFilter from './filter/currency'
+window.Vue = Vue
 
-import "bootstrap";
-import "bootstrap/dist/css/bootstrap.min.css";
-import Loading from "vue-loading-overlay";
-import "vue-loading-overlay/dist/vue-loading.css";
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Loading from 'vue-loading-overlay'
+import 'vue-loading-overlay/dist/vue-loading.css'
 
-import "swiper/css/swiper.css";
-import VueAwesomeSwiper from "vue-awesome-swiper";
-Vue.use(VueAwesomeSwiper);
+import 'swiper/css/swiper.css'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+Vue.use(VueAwesomeSwiper)
 
-Vue.use(VueAxios, axios);
+Vue.use(VueAxios, axios)
 
-Vue.component("Loading", Loading); // loading 全域啟用
+Vue.component('Loading', Loading) // loading 全域啟用
 
-Vue.filter("currency", currencyFilter);
+Vue.filter('currency', currencyFilter)
 //這樣所有頁面只要加上( | currency ) ，就套上千分號了。
-Vue.config.productionTip = false;
+Vue.config.productionTip = false
 
-axios.defaults.withCredentials = true;
+axios.defaults.withCredentials = true
 /* eslint-disable no-new */
 new Vue({
-  el: "#app",
+  el: '#app',
   router,
-  render: h => h(App)
-});
+  render: h => h(App),
+})
