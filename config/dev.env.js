deca34815a1ddad8d5b3e4608f6c2177be2f51b4
PKTseng <jk0910948659@Gmail.com>
2021-03-19 13:55:19 +0800

    delete url

--- config/dev.env.js
+++ config/dev.env.js
@@ -1,9 +1,7 @@
-"use strict";
-const merge = require("webpack-merge");
-const prodEnv = require("./prod.env");
+'use strict'
+const merge = require('webpack-merge')
+const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  THEOAPI: '"http://57a627b7dc91.ngrok.io"'
-  // 這是伺服器路徑
-});
+})
