2fe4edd52f58346b31038fc49cfaf688baa1da69
PKTseng <jk0910948659@Gmail.com>
2021-03-21 11:38:16 +0800

    Refactor button and add style

--- src/components/MovieCard/MovieCard.vue
+++ src/components/MovieCard/MovieCard.vue
@@ -1,41 +1,35 @@
 <template>
   <div>
-    <div class="col-md-6 mb-4 col-lg-4" v-for="(item, key) in info" :key="key">
-      <div class="card h-100 border-0 bg-dark text-light cardTitle">
-        <div class="type">{{ item.product_type }}</div>
-        <router-link
-          :to="{
-            name: 'MovieInfo',
-            params: {
-              id: item.product_id,
-              img: item.image_url,
-              name: item.product_name,
-              type: item.product_type,
-              description: item.description,
-              price: item.price,
-              score: item.movie_score,
-              time: item.movie_runtime,
-            },
-          }"
-          class="thumbnail"
-          ><img :src="item.image_url" class="card-img-top" alt="..."
-        /></router-link>
+    <div class="card h-100 border-0 bg-dark text-light cardTitle">
+      <div class="type">{{ item.product_type }}</div>
+      <router-link
+        :to="{
+          name: 'MovieInfo',
+          params: {
+            id: item.product_id,
+            img: item.image_url,
+            name: item.product_name,
+            type: item.product_type,
+            description: item.description,
+            price: item.price,
+            score: item.movie_score,
+            time: item.movie_runtime,
+          },
+        }"
+        class="thumbnail"
+        ><img :src="item.image_url" class="card-img-top" alt="..."
+      /></router-link>
 
-        <div
-          class="card-body d-flex justify-content-between align-items-center"
-        >
-          <h4 class="m-0">{{ item.product_name }}</h4>
-        </div>
-        <div class="px-2 d-flex flex-column border-white">
-          <button class="btn btn-outline-light btn-sm mb-2">
-            加入我的最愛
-          </button>
-          <button class="btn btn-outline-light btn-sm mb-2">我要租借</button>
-          <button class="btn btn-outline-danger btn-sm mb-2">
-            <i class="fas fa-spinner fa-spin" v-if="status.addLoading"></i>
-            我要購買
-          </button>
-        </div>
+      <div class="card-body d-flex justify-content-between align-items-center">
+        <h4 class="m-0 movieName">{{ item.product_name }}</h4>
+      </div>
+      <div class="px-2 d-flex flex-column border-white">
+        <button class="btn btn-outline-light btn-sm mb-2">加入我的最愛</button>
+
+        <button class="btn btn-outline-danger btn-sm mb-2">
+          <i class="fas fa-spinner fa-spin" v-if="status.addLoading"></i>
+          我要購買
+        </button>
       </div>
     </div>
   </div>
@@ -46,11 +40,69 @@ export default {
   name: 'MovieCard',
   data() {
     return {
-      info: [],
+      status: {
+        addLoading: false,
+      },
     }
   },
-  props: ['info'],
+  props: {
+    item: {},
+  },
 }
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+// 卡片
+.cardTitle {
+  position: relative;
+  overflow: hidden;
+  border: 0;
+  .thumbnail {
+    overflow: hidden;
+    img {
+      transform: scale(1, 1);
+      transition: 0.5s;
+    }
+    img:hover {
+      transform: scale(1.1, 1.1);
+    }
+  }
+}
+
+// 卡片標籤
+.type {
+  z-index: 1;
+  font-weight: 700;
+  padding: 5px;
+  width: 120px;
+  left: -30px;
+  top: 10px;
+  text-align: center;
+  position: absolute;
+  background: #ee5a24;
+  transform: rotate(-45deg);
+  font-size: 18px;
+}
+// 手機板固定在下方的選單
+.mobileMenu {
+  position: fixed;
+  bottom: 0;
+  display: none;
+  @media (max-width: 768px) {
+    display: block;
+    z-index: 2;
+  }
+}
+
+.movieInfo {
+  @media (max-width: 768px) {
+    margin-top: 50px;
+  }
+}
+
+.mobile {
+  @media (max-width: 768px) {
+    display: none;
+  }
+}
+</style>
