cfe8befcc1113fb22493449a6b6be65df7a843f8
PKTseng <jk0910948659@Gmail.com>
2021-03-21 18:35:09 +0800

    Add orogin web

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -4,15 +4,17 @@
       <div class="container"></div>
       <ul class="list-inline d-flex justify-content-center align-items-center">
         <li class="list-inline-item">
-          © Copyright 2021 平價電影出租純屬練習作品
+          © Copyright 2021 平價電影出租純屬練習作品，無任何商業用途
         </li>
-        <!-- <li class="list-inline-item">
-          <a href="javascript::" class="text-info"></a>
-        </li>
-        <li class="list-inline-item">
-          <a href="javascript::" class="text-info"></a>
-        </li> -->
       </ul>
+      <div class="text-center">
+        此作品的版型參考自 :
+        <a
+          href="https://gx7879.github.io/vue-dashboard-demo/dist/#/"
+          target="_blank"
+          >週末電影院</a
+        >
+      </div>
     </footer>
   </div>
 </template>
