43c57a42d4b1cf3c6ef3befb680122cd5e151615
PKTseng <jk0910948659@Gmail.com>
2021-03-24 22:48:01 +0800

    refactor style

--- src/views/MovieInfo.vue
+++ src/views/MovieInfo.vue
@@ -3,8 +3,8 @@
     <Navbar />
     <section class="container mt-5">
       <div class="row text-white">
-        <div class="col-md-5 movie d-flex justify-content-end h-100">
-          <img :src="movieInfo.img" alt="" />
+        <div class="col-md-5 d-flex justify-content-center">
+          <img :src="movieInfo.img" alt="" class="movieImg" />
         </div>
         <div class="col-md-5 d-flex flex-column justify-content-lg-between">
           <div class="movieInfo">
@@ -36,16 +36,14 @@
                 <strong>{{ movieInfo.price | currency }} </strong>
               </div>
             </div>
-            <div
-              class="d-flex justify-content-end align-items-end mt-3 btn-group"
-            >
-              <button class="btn btn-outline-light">
+            <div class="mobileBtn d-flex justify-content-end mt-3">
+              <button class="btn btn-outline-light mr-3 p-2">
                 <i class="far fa-heart"></i>
-                <span class="px-1 mobile">加到我的最愛</span>
+                <span class="px-1">加到我的最愛</span>
               </button>
-              <button class="btn btn-outline-light">
+              <button class="btn btn-outline-light mr-3 p-2">
                 <i class="fas fa-cart-arrow-down"></i>
-                <span class="px-1 mobile">加入購物車</span>
+                <span class="px-1">加入購物車</span>
               </button>
             </div>
           </div>
@@ -90,6 +88,10 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.movieImg {
+  height: 500px;
+}
+
 .description {
   font-weight: 500;
   font-size: 16px;
@@ -118,6 +120,12 @@ export default {
 .movieInfo {
   @media (max-width: 768px) {
     margin-top: 50px;
+  }
+}
+
+.mobileBtn {
+  @media (max-width: 1024px) {
+    margin-bottom: 100px;
   }
 }
 </style>
