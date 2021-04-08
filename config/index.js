bed7aa5c5a20a465432b0fea2ba32af276611aa8
PKTseng <jk0910948659@Gmail.com>
2021-04-08 21:10:50 +0800

    add movie V3

--- config/index.js
+++ config/index.js
@@ -50,8 +50,8 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: './',
-    // assetsPublicPath: '/movie/dist/',
+    // assetsPublicPath: '/',
+    assetsPublicPath: '/movieV3/',
 
     /**
      * Source Maps
