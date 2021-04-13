7efed88a448ebf0270aa2b002f9f2cd6c49ced40
PKTseng <jk0910948659@Gmail.com>
2021-04-14 00:06:36 +0800

    fix product id

--- config/prod.env.js
+++ config/prod.env.js
@@ -2,5 +2,5 @@
 module.exports = {
   NODE_ENV: '"production"',
   USERAPI:
-    '"http://ec2-35-73-107-34.ap-northeast-1.compute.amazonaws.com:8080/"',
+    '"https://ec2-35-73-107-34.ap-northeast-1.compute.amazonaws.com:8080/"',
 }
